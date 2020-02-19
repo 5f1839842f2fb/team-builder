12e0e190cfc3eb5f04e2011ecff83f043faf6c86
5f1839842f2fb <57242175+5f1839842f2fb@users.noreply.github.com>
2020-02-19 17:36:37 -0500

    Base list

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Reactstrap from 'reactstrap';
 import logo from './logo.svg';
 import './App.css';
+import TeamMemberCard from './TeamMemberCard'
 
 function App() {
+  const [team, setTeam] = useState([{
+    name: 'Ted Smith',
+    email: 'tedsmith@fakemail.org',
+    role: 'Fake-end Fakegineer'
+  },
+  {
+    name: 'Rob Fulmer',
+    email: 'robfulmer@fakemail.org',
+    role: 'Fake-end Fakegineer'
+  }])
+
   return (
     <div className="App">
-      asdasd
+      <div className="cardDiv">
+      {team.map((element) => {
+        return <TeamMemberCard teamMember={element} key={Math.random()}/>
+      })}
+      </div>
+      
     </div>
   );
 }
